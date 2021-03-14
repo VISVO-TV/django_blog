07863e8944f98a8b8b3883e73b5f298089b0051f
visvo-tv <visvo.lider@gmail.com>
2021-03-14 16:23:24 +0300

    add basic functionality for blog

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,80 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View
+from django.urls import reverse
+from .utils import *
+from .models import Post, Tag
+from .forms import TagForm, PostForm
 
-# Create your views here.
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
 def posts_list(request):
-    n = ['Dasha', 'Dima', 'Roma', 'Vera']
-    return render(request, 'blog/index.html', context={'names': n})
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    return render(request, 'blog/index.html', context={'page': page})
+
+
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create.html'
+    raise_exception = True
+
+
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    raise_exception = True
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    form_model = PostForm
+    template = 'blog/post_update.html'
+    raise_exception = True
+
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    form_model = TagForm
+    template = 'blog/tag_update.html'
+    raise_exception = True
+
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    page_transition = 'posts_list_url'
+    raise_exception = True
+
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    page_transition = 'tags_list_url'
+    raise_exception = True
